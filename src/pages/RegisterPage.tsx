97a2b0eb777ac28305dd91076003319ced5478ea
Sot. Tzelatidis <stzelati@gmail.com>
2025-09-11 20:29:22 +0300

    feat: Login and Register page is now mobile responsive

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -40,74 +40,77 @@ const RegisterPage = () => {
 
   return (
     <>
-      <form
-        className="max-w-sm mx-auto p-8 space-y-4 mt-20 rounded-xl bg-white shadow-xl"
-        onSubmit={handleSubmit(onSubmit)}
+      <div
+        className={"flex justify-center px-4 sm:px-0"}
       >
-        {errors.root && <div className={"p-2 mt-2 bg-red-50 border-l-4 border-red-500 text-red-700 rounded"}>{errors.root.message}</div>}
-        <h3 className="text-xl font-semibold text-center text-gray-600">Welcome!</h3>
-        <p className="mt-1 text-center text-gray-500 ">Create an account!</p>
-        <div className={"space-y-2"}>
-          <div>
-            <Label htmlFor="username" className="mb-1"></Label>
-            <Input
-              {...register("username")}
-              id="username"
-              type={"username"}
-              placeholder={"Username"}
-            />
+        <form
+          className="w-full max-w-sm mx-auto p-5 sm:p-8 space-y-4 mt-16 sm:mt-20 rounded-xl bg-white shadow-xl"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          {errors.root && <div className={"p-2 mt-2 bg-red-50 border-l-4 border-red-500 text-red-700 rounded"}>{errors.root.message}</div>}
+          <h3 className="text-xl font-semibold text-center text-gray-600">Welcome!</h3>
+          <p className="mt-1 text-center text-gray-500 ">Create an account!</p>
+          <div className={"space-y-2"}>
+            <div>
+              <Label htmlFor="username" className="mb-1"></Label>
+              <Input
+                {...register("username")}
+                id="username"
+                type={"username"}
+                placeholder={"Username"}
+              />
+            </div>
+            {errors.username && <div className={"p-2 mt-2 bg-red-50 border-l-4 border-red-500 text-red-700 rounded"}>{errors.username.message}</div>}
+            <div>
+              <Label htmlFor="firstname" className="mb-1"></Label>
+              <Input
+                {...register("firstname")}
+                id="firstname"
+                type="firstname"
+                placeholder={"First Name"}
+              />
+            </div>
+            {errors.firstname && <div className={"p-2 mt-2 bg-red-50 border-l-4 border-red-500 text-red-700 rounded"}>{errors.firstname.message}</div>}
+            <div>
+              <Label htmlFor="lastname" className="mb-1"></Label>
+              <Input
+                {...register("lastname")}
+                id="lastname"
+                type="lastname"
+                placeholder={"Last Name"}
+              />
+            </div>
+            {errors.lastname && <div className={"p-2 mt-2 bg-red-50 border-l-4 border-red-500 text-red-700 rounded"}>{errors.lastname.message}</div>}
+            <div>
+              <Label htmlFor="password" className="mb-1"></Label>
+              <Input
+                {...register("password")}
+                id="password"
+                type="password"
+                placeholder={"Password"}
+              />
+              {errors.password && <div className={"p-2 mt-2 bg-red-50 border-l-4 border-red-500 text-red-700 rounded"}>{errors.password.message}</div>}
+              <a onClick={() => {navigate("/login")}} className={"text-xs text-gray-400 ms-1 hover:text-black"}>Already have an Account?</a>
+            </div>
           </div>
-          {errors.username && <div className={"p-2 mt-2 bg-red-50 border-l-4 border-red-500 text-red-700 rounded"}>{errors.username.message}</div>}
-          <div>
-            <Label htmlFor="firstname" className="mb-1"></Label>
-            <Input
-              {...register("firstname")}
-              id="firstname"
-              type="firstname"
-              placeholder={"First Name"}
-            />
-          </div>
-          {errors.firstname && <div className={"p-2 mt-2 bg-red-50 border-l-4 border-red-500 text-red-700 rounded"}>{errors.firstname.message}</div>}
-          <div>
-            <Label htmlFor="lastname" className="mb-1"></Label>
-            <Input
-              {...register("lastname")}
-              id="lastname"
-              type="lastname"
-              placeholder={"Last Name"}
-            />
-          </div>
-          {errors.lastname && <div className={"p-2 mt-2 bg-red-50 border-l-4 border-red-500 text-red-700 rounded"}>{errors.lastname.message}</div>}
-          <div>
-            <Label htmlFor="password" className="mb-1"></Label>
-            <Input
-              {...register("password")}
-              id="password"
-              type="password"
-              placeholder={"Password"}
-            />
-            {errors.password && <div className={"p-2 mt-2 bg-red-50 border-l-4 border-red-500 text-red-700 rounded"}>{errors.password.message}</div>}
-            <a onClick={() => {navigate("/login")}} className={"text-xs text-gray-400 ms-1 hover:text-black"}>Already have an Account?</a>
-          </div>
-        </div>
 
-        <div className={"flex flex-col items-center justify-center space-y-2"}>
-          <Button
-            type="submit"
-            disabled={isSubmitting}
-          >
-            {isSubmitting ? "Creating account..." : "Create an account"}
-          </Button>
-          <Button
-            variant={"outline"}
-            className=""
-            onClick={onClickBack}
-          >
-            Back
-          </Button>
-        </div>
-
-      </form>
+          <div className={"flex flex-col items-center justify-center space-y-2"}>
+            <Button
+              type="submit"
+              disabled={isSubmitting}
+            >
+              {isSubmitting ? "Creating account..." : "Create an account"}
+            </Button>
+            <Button
+              variant={"outline"}
+              className=""
+              onClick={onClickBack}
+            >
+              Back
+            </Button>
+          </div>
+        </form>
+      </div>
     </>
   )
 }
