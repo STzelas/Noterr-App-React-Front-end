c8a9ac5ac2ed501ab5bdfcd8bdb41ef10733b419
Sot. Tzelatidis <stzelati@gmail.com>
2025-10-22 18:06:59 +0300

    small updates

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -42,56 +42,52 @@ export default function LoginPage() {
 
   return (
     <>
-      <div
-        className={"flex justify-center px-4 sm:px-0"}
+
+      <form
+        className="max-w-sm mx-auto p-8 space-y-4 mt-20 rounded-xl bg-white shadow-xl"
+        onSubmit={handleSubmit(onSubmit)}
       >
-        <form
-          className="w-full max-w-sm sm:max-w-sm mx-auto p-6 sm:p-8 space-y-4 mt-16 sm:mt-20 rounded-xl bg-white shadow-xl"
-          onSubmit={handleSubmit(onSubmit)}
-        >
-          {errors.root && <div className={"p-2 mt-2 bg-red-50 border-l-4 border-red-500 text-red-700 rounded"}>{errors.root.message}</div>}
-          <h3 className="text-xl font-semibold text-center text-gray-600">Welcome!</h3>
-          <p className="mt-1 text-center text-gray-500 ">Login or create account</p>
-          <div>
-            <Label htmlFor="username" className="mb-1"></Label>
-            <Input
-              {...register("username")}
-              id="username"
-              placeholder={"Username"}
-            />
-            {errors.username && <div className={"p-2 mt-2 bg-red-50 border-l-4 border-red-500 text-red-700 rounded"}>{errors.username.message}</div>}
-          </div>
+        {errors.root && <div className={"p-2 mt-2 bg-red-50 border-l-4 border-red-500 text-red-700 rounded"}>{errors.root.message}</div>}
+        <h3 className="text-xl font-semibold text-center text-gray-600">Welcome!</h3>
+        <p className="mt-1 text-center text-gray-500 ">Login or create account</p>
+        <div>
+          <Label htmlFor="username" className="mb-1"></Label>
+          <Input
+            {...register("username")}
+            id="username"
+            placeholder={"Username"}
+          />
+          {errors.username && <div className={"p-2 mt-2 bg-red-50 border-l-4 border-red-500 text-red-700 rounded"}>{errors.username.message}</div>}
+        </div>
 
-          <div>
-            <Label htmlFor="password" className="mb-1"></Label>
-            <Input
-              {...register("password")}
-              id="password"
-              type="password"
-              placeholder={"Password"}
-            />
-            {errors.password && <div className={"p-2 mt-2 bg-red-50 border-l-4 border-red-500 text-red-700 rounded"}>{errors.password.message}</div>}
-          </div>
-          <div className={"flex flex-col items-center justify-center space-y-2"}>
-            <Button
-              type="submit"
-              disabled={isSubmitting}
-            >
-              {isSubmitting ? "Logging in..." : "Login"}
-            </Button>
-            <Button
-              variant={"outline"}
-              className=""
-              onClick={onRegisterRedirect}
-            >
-              Create an account
-            </Button>
-            <button onClick={(e) => {e.preventDefault(); navigate("/")}} className={"text-xs text-gray-400 ms-1 hover:text-black"}>or go Back</button>
-          </div>
+        <div>
+          <Label htmlFor="password" className="mb-1"></Label>
+          <Input
+            {...register("password")}
+            id="password"
+            type="password"
+            placeholder={"Password"}
+          />
+          {errors.password && <div className={"p-2 mt-2 bg-red-50 border-l-4 border-red-500 text-red-700 rounded"}>{errors.password.message}</div>}
+        </div>
+        <div className={"flex flex-col items-center justify-center space-y-2"}>
+          <Button
+            type="submit"
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? "Logging in..." : "Login"}
+          </Button>
+          <Button
+            variant={"outline"}
+            className=""
+            onClick={onRegisterRedirect}
+          >
+            Create an account
+          </Button>
+          <button onClick={(e) => {e.preventDefault(); navigate("/")}} className={"text-xs text-gray-400 ms-1 hover:text-black"}>or go Back</button>
+        </div>
 
-        </form>
-      </div>
-
+      </form>
     </>
   );
 }
